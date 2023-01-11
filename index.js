65f2d8901a347f1945920968009bd9e97640bea5
Garrett <gcamoin5@gmail.com>
2023-01-11 13:40:41 -0500

    commit tree d9d9347756a7671c3e98c4f6fb9c7106a1fa595f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+ const result = record.find(element => element["result"] === 'W') 
+ if (result === undefined)
+ { return undefined
+ }
+ return result["year"]
+  }    
